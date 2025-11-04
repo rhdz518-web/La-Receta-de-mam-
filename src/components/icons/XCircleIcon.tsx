a133ac71f2eb1b3f1ddd0756bc53f1926ad84076
rhdz518-web <r.h.d.z518@gmail.com>
2025-11-04 04:42:40 -0600

    Corrigiendo la estructura de carpetas y componentes

--- src/components/icons/XCircleIcon.tsx
+++ src/components/icons/XCircleIcon.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const ClockIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+const XCircleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -13,9 +13,9 @@ const ClockIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     />
   </svg>
 );
 
-export default ClockIcon;
+export default XCircleIcon;
